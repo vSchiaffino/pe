146bd21ba8caa417b52a1c5e72ea0ccc423487dd
Valentin Schiaffino <valenschg@gmail.com>
2022-03-18 10:54:16 -0300

    ej2

---
+++ 1/ej2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int operacion(int a, int b, char op)
+{
+    switch (op)
+    {
+    case '+':
+        return a + b;
+        break;
+    case '-':
+        return a - b;
+        break;
+    case '*':
+        return a * b;
+        break;
+    case '/':
+        return a / b;
+        break;
+    default:
+        break;
+    }
+}
+
+int main()
+{
+    int a, b;
+    char op;
+    printf("Ingrese primer numero: ");
+    scanf("%d", &a);
+    printf("Ingrese segundo numero: ");
+    scanf("%d", &b);
+    printf("Ingrese operacion[+ - * /]: ");
+    fflush(stdin);
+    op = getchar();
+
+    printf("%d %c %d = %d", a, op, b, operacion(a, b, op));
+    return 0;
+}
