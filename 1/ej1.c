f44e21fe812f056f630f40d9d30044b499fb1d33
Valentin Schiaffino <valenschg@gmail.com>
2022-03-17 23:34:19 -0300

    ej1

--- 1/ej1.c
+++ 1/ej1.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 
+int redondeo(float n)
+{
+    int in = n;
+    float decimal = n - in;
+    if (decimal >= .5)
+    {
+        n++;
+    }
+    return n;
+}
+
+int piso(float n)
+{
+    int in = n;
+    return in;
+}
+
+int techo(float n)
+{
+    int in = n;
+    float decimal = n - in;
+    if (decimal != 0)
+    {
+        n++;
+    }
+    return n;
+}
+
 int main()
 {
-    printf("hello worldddd");
+    float n;
+    printf("Ingrese un numero reall: ");
+    scanf("%f", &n);
+
+    printf("Redondeo = %d", redondeo(n));
+    printf("\nPiso = %d", piso(n));
+    printf("\nTecho = %d", techo(n));
+
     return 0;
 }
