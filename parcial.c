212a84f0985bb061469fe45b2841aa434e3049c4
Valentin Schiaffino <valenschg@gmail.com>
2022-05-13 11:15:16 -0300

    Update parcial.c

--- parcial.c
+++ parcial.c
@@ -1,10 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define FITEMS "1/items.txt"
 #define FDATOS "1/datos.txt"
 
-struct s_fila{
-    char* equipo;
+struct s_string
+{
+    char *arreglo;
+    int size;
+};
+
+typedef struct s_string String;
+
+struct s_fila
+{
+    String equipo;
+    int pts;
+    int pj;
+    int pg;
+    int pe;
+    int pp;
+    int gf;
+    int gc;
+    int dif;
+};
+
+typedef struct s_fila Fila;
+
+struct s_filas
+{
+    Fila *arreglo;
+    int size;
+};
+typedef struct s_filas Filas;
+
+String crearString()
+{
+    String string;
+    string.arreglo = (char *)malloc(sizeof(char));
+    *string.arreglo = '\0';
+    string.size = 0;
+    return string;
+}
+
+String limpiarString(String *string)
+{
+    string->size = 0;
+    string->arreglo = realloc(string->arreglo, sizeof(char));
+    string->arreglo[0] = '\0';
+}
+
+void append(String *string, char c)
+{
+    printf("intento apendear string: %c -", c);
+    string->size++;
+    // falta un +1 en el realloc?
+    string->arreglo = (char *)realloc(string->arreglo, sizeof(char) * (string->size));
+    char *puntero = string->arreglo;
+    while (*puntero != '\0')
+    {
+        puntero++;
+    }
+    *puntero = c;
+    *(puntero + 1) = '\0';
+    printf("termine\n", c);
+}
+
+Filas crearFilas()
+{
+    Filas filas;
+    filas.arreglo = malloc(1);
+    filas.size = 0;
+    return filas;
+}
+
+void appendFila(Filas *filas, Fila fila)
+{
+    filas->size++;
+    filas->arreglo = realloc(filas->arreglo, sizeof(Fila) * filas->size);
+    char *nuevoString = malloc(sizeof(char) * filas->arreglo->equipo.size + 1);
+    strcpy(nuevoString, fila.equipo.arreglo);
+    fila.equipo.arreglo = nuevoString;
+    filas->arreglo[filas->size - 1] = fila;
+}
+
+void mostrarFila(Fila fila)
+{
     int pts;
     int pj;
     int pg;
@@ -13,64 +94,62 @@ struct s_fila{
     int gf;
     int fc;
     int dif;
-};
-
-typedef struct s_fila Fila;
-
-Fila* cargarEquipos()
-{
-    Fila* filas = malloc(1);
-    FILE* fitems = fopen(FITEMS, "r");
-    FILE* fdatos = fopen(FDATOS, "r");
-    int i = 0;
-    while(!feof(fitems))
-    {
-        Fila fila;
-        fila.equipo = malloc(1);
-        filas = realloc(filas, (i+1) * sizeof(Fila));
-        int j = 0;
-        char aux = 'c';
-        while(!feof(fitems))
-        {
-            aux = fgetc(fitems);
-            fila.equipo = realloc(fila.equipo, (j+1) * sizeof(char));
-            fila.equipo[j] = aux;
-            ++j;
-        }
-        char aux = 'c';
-        while(!feof(fdatos))
-        {
-            fscanf("%d,%d,%d")
-        }
-        fila.equipo = realloc(fila.equipo, (j) * sizeof(char));
-        fila.equipo[j-1] = '\0';
-        filas = realloc(filas, (i+1)*sizeof(Fila));
-        filas[i] = fila;
-        i++;
-    }
-    Fila fila;
-    fila.pts = -1;
-    filas = realloc(filas, (i+1)*sizeof(Fila));
-    filas[i] = fila;
-
-    return filas;
+    printf("%-15s %4d %4d %4d %4d %4d %4d %4d %4d\n", fila.equipo.arreglo, fila.pts, fila.pj, fila.pg, fila.pe, fila.pp, fila.gf, fila.gc, fila.dif);
 }
 
-void mostrarEquipos(Fila* filas)
+void mostrarFilas(Filas filas)
 {
-    int i = 0;
-    while (filas[i].pts != -1)
+    printf("%-15s %4s %4s %4s %4s %4s %4s %4s %4s\n", "Equipo", "PTS", "PJ", "PG", "PE", "PP", "GF", "GC", "DIF");
+    for (int i = 0; i < 55; i++)
     {
-        printf("%s\n", filas[i].equipo);
-        ++i;
+        printf("-");
     }
-    
+    printf("\n");
+    for (int i = 0; i < filas.size; i++)
+    {
+        mostrarFila(filas.arreglo[i]);
+    }
+}
+
+void cargarItems(Filas *filas)
+{
+    FILE *f = fopen(FITEMS, "r");
+    String string = crearString();
+    char aux;
+    while (aux != EOF)
+    {
+        aux = fgetc(f);
+        if (aux == '\n')
+        {
+            Fila fila = {string, 0, 0, 0, 0, 0, 0, 0, 0};
+            appendFila(filas, fila);
+            limpiarString(&string);
+        }
+        else if (aux != EOF)
+        {
+            append(&string, aux);
+        }
+    }
+}
+
+void cargarDatos(Filas *filas)
+{
+    FILE *f = fopen(FDATOS, "r");
+    int i = 0;
+    while (i < filas->size && !feof(f))
+    {
+        Fila *fila = &filas[0].arreglo[i];
+        fscanf(f, "%d,%d,%d,%d,%d,%d,%d,%d\n", &fila->pts, &fila->pj, &fila->pg, &fila->pe, &fila->pp, &fila->gf, &fila->gc, &fila->dif);
+        i++;
+    }
 }
 
 int main()
 {
-    Fila* filas = cargarEquipos();
-    mostrarEquipos(filas);
-
+    Filas filas = crearFilas();
+    cargarItems(&filas);
+    cargarDatos(&filas);
+    mostrarFilas(filas);
+    printf("sali bien");
     return 0;
 }
