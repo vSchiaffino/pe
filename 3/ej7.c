3ebb48deb02487840f04f998457beaf5c715cfac
Valentin Schiaffino <valenschg@gmail.com>
2022-05-27 10:46:53 -0300

    Create ej7.c

---
+++ 3/ej7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *cargarStrDin()
+{
+    char *puntero = malloc(1);
+    int i = 0;
+    char aux = 'c';
+    while (aux != '\n')
+    {
+        ++i;
+        aux = getchar();
+        puntero = realloc(puntero, i * sizeof(char));
+        puntero[i - 1] = aux;
+    }
+    puntero[i - 1] = '\0';
+    return puntero;
+}
+
+int main()
+{
+    char *str = cargarStrDin();
+    printf("Cargado: %s", str);
+}
