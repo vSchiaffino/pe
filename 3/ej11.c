7f229bd7ae334223ce3dc7bed47ec15317c76f2d
Valentin Schiaffino <valenschg@gmail.com>
2022-06-08 17:17:47 -0300

    Create ej11.c

---
+++ 3/ej11.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int a, b;
+} Dosint;
+
+void intercambiar(int *ptr1, int *ptr2)
+{
+    int aux = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = aux;
+}
+
+int main()
+{
+    Dosint dosint = {4, 5};
+    printf("Antes: %d - %d\n", dosint.a, dosint.b);
+    intercambiar(&dosint.a, &dosint.b);
+    printf("Despues: %d - %d\n", dosint.a, dosint.b);
+}
