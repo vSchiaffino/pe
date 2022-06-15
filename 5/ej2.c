efa9c384be4f8592e363e7192eda881a31a12025
Valentin Schiaffino <valenschg@gmail.com>
2022-06-15 01:42:28 -0300

    Update ej2.c

--- 5/ej2.c
+++ 5/ej2.c
@@ -1,77 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct s_lista
+typedef struct s_nodo
 {
-    int len;
-    int *array;
-} Lista;
+    int valor;
+    struct s_nodo* next;
+} Nodo;
 
-void insert(Lista *lista, int posicion, int valor)
-{
-    if (lista->len == 0)
-    {
-        lista->array = malloc(sizeof(int) * 1);
-    }
-    else
-    {
-        lista->array = realloc(lista->array, sizeof(int) * (lista->len + 1));
-    }
-    int aux = valor;
-    for (int i = posicion; i < lista->len + 1; i++)
-    {
-        int aux2 = lista->array[i];
-        lista->array[i] = aux;
-        aux = aux2;
-    }
-
-    lista->len++;
-}
-
-void insertarOrdenado(Lista *lista, int valor)
+void insert(Nodo lista, int posicion, int valor)
 {
     int i = 0;
-    while (i < lista->len && lista->array[i] < valor)
+    Nodo actual = lista;
+    while(i < posicion && actual.next != NULL)
     {
+        actual = *actual.next;
         i++;
     }
-    insert(lista, i, valor);
-}
-
-void mostrarLista(Lista lista)
-{
-    printf("[");
-    for (int i = 0; i < lista.len; i++)
-    {
-        printf("%d", lista.array[i]);
-        if (i != lista.len - 1)
-        {
-            printf(",");
-        }
-    }
-    printf("]\n");
 }
 
 int main()
 {
-    Lista lista;
-    lista.len = 0;
-    insertarOrdenado(&lista, 5);
-    mostrarLista(lista);
-    insertarOrdenado(&lista, 1);
-    mostrarLista(lista);
-    insertarOrdenado(&lista, 4);
-    mostrarLista(lista);
-    insertarOrdenado(&lista, 2);
-    mostrarLista(lista);
-    insertarOrdenado(&lista, 3);
-    mostrarLista(lista);
-    insertarOrdenado(&lista, -1);
-    mostrarLista(lista);
-    insertarOrdenado(&lista, 154);
-    mostrarLista(lista);
-    insertarOrdenado(&lista, 6);
-    mostrarLista(lista);
-    printf("salgo bien");
     return 0;
 }
